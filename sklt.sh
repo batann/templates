b7429ba9535d3cf756992282013bcc4444dbe32f
batann <122584289+batann@users.noreply.github.com>
2024-12-05 02:06:57 +1100

    Add files via upload

---
+++ sklt.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# vim:fileencoding=utf-8:foldmethod=marker
+#author:	fairdinkum batan
+#mail:		12982@tutanota.com
+------------------------------------------------------------------------------------------------
+
+#{{{###  clear command and ansi coloring
+clear
+Black='\033[0;30m'
+Red='\033[0;31m'
+Green='\033[0;32m'
+Blue='\033[0;34m'
+Purple='\033[0;35m'
+Cyan='\033[0;36m'
+Yellow='\033[1;33m'
+White='\033[1;37m'
+BBlue='\e[0;104m'
+BBlack='\e[0;100m'
+RRed='\e[0;100m'
+GGreen='\e[0;100m'
+YYellow='\e[0;100m'
+BBlue='\e[0;100m'
+PPurple='\e[0;100m'
+CCyan='\e[0;100m'
+WWhite='\e[0;100m'
+
+#}}}
+#{{{###
+
+#}}}
+
+
+
